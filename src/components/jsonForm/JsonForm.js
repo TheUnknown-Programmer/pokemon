f99d2cf83b2ce1ac862021570e9ad52bd9d76382
f.jonathan9700@gmail.com <106790970+TheUnknown-Programmer@users.noreply.github.com>
2023-10-19 17:41:46 -0400

    Touched up the styling to center everything/ Added a contact button to redirect the user to the contact page

--- src/components/jsonForm/JsonForm.js
+++ src/components/jsonForm/JsonForm.js
@@ -1,56 +1,71 @@
-import React, { useState } from 'react';
-import styles from './JsonForm.module.css';
+import React, { useState } from "react";
+import styles from "./JsonForm.module.css";
 
 function JsonForm() {
-    const [formData, setFormData] = useState({
-        name: '',
-        email: ''
-    });
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+  });
 
-    const handleChange = (e) => {
-        const { name, value } = e.target;
-        setFormData(prevState => ({
-            ...prevState,
-            [name]: value
-        }));
-    };
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        
-        try {
-            const response = await fetch('https://jsonplaceholder.typicode.com/posts', {
-                method: 'POST',
-                body: JSON.stringify(formData),
-                headers: {
-                    'Content-type': 'application/json; charset=UTF-8'
-                },
-            });
-            const json = await response.json();
-            console.log(json);  // Response from the server
-        } catch (error) {
-            console.error('Error:', error);
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await fetch(
+        "https://jsonplaceholder.typicode.com/posts",
+        {
+          method: "POST",
+          body: JSON.stringify(formData),
+          headers: {
+            "Content-type": "application/json; charset=UTF-8",
+          },
         }
-    };
+      );
+      const json = await response.json();
+      console.log(json); // Response from the server
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
 
-    return (
-        <div className={styles.formContainer}>
-            <form onSubmit={handleSubmit} className={styles.form}>
-                <div className={styles.formGroup}>
-                    <label className={styles.label}>Name:</label>
-                    <input type="text" name="name" value={formData.name} onChange={handleChange} className={styles.input} />
-                </div>
-                <div className={styles.formGroup}>
-                    <label className={styles.label}>Email:</label>
-                    <input type="email" name="email" required value={formData.email} onChange={handleChange} className={styles.input} />
-                </div>
-                <button type="submit" className={styles.submitButton}>Submit</button>
-            </form>
-
-            
+  return (
+    <div className={styles.formContainer}>
+      <form onSubmit={handleSubmit} className={styles.form}>
+        <div className={styles.formGroup}>
+          <label className={styles.label}>Name:</label>
+          <input
+            type="text"
+            name="name"
+            value={formData.name}
+            onChange={handleChange}
+            className={styles.input}
+          />
         </div>
-    );
-    
+        <div className={styles.formGroup}>
+          <label className={styles.label}>Email:</label>
+          <input
+            type="email"
+            name="email"
+            required
+            value={formData.email}
+            onChange={handleChange}
+            className={styles.input}
+          />
+        </div>
+        <button type="submit" className={styles.submitButton}>
+          Submit
+        </button>
+      </form>
+    </div>
+  );
 }
 
 export default JsonForm;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,15 @@
-import React from 'react';
-import PokemonDataTable from '../components/PokemonDataTable';
-import Logo1 from '../assets/pokemon.png'
-import Image from 'next/image'
+import React from "react";
+import PokemonDataTable from "../components/PokemonDataTable";
+import Logo from "../assets/pokemon.png";
+import Image from "next/image";
 
 function HomePage() {
   return (
     <div>
       <div className="logo-container">
-        <Image src={Logo1} alt="Pokémon" className="logo-image" />
+        <Image src={Logo} alt="Pokémon" className="logo-image" />
       </div>
       <PokemonDataTable />
-      
     </div>
   );
 }
