7901ec3e3fe157ca9016d18575d7574a538e0f24
f.jonathan9700@gmail.com <106790970+TheUnknown-Programmer@users.noreply.github.com>
2023-10-19 19:26:19 -0400

    added a stock image to empty image api

--- src/components/PokemonDataTable.js
+++ src/components/PokemonDataTable.js
@@ -23,7 +23,14 @@ const columns = [
     headerName: <CatchingPokemonOutlinedIcon className="centerIcon" />,
     width: 150,
     renderCell: (params) => (
-      <img src={params.value} alt="Pokemon" style={{ width: 50, height: 50 }} />
+      <img
+        src={
+          params.value ||
+          "https://thenounproject.com/api/private/icons/640375/edit/?backgroundShape=SQUARE&backgroundShapeColor=%23000000&backgroundShapeOpacity=0&exportSize=752&flipX=false&flipY=false&foregroundColor=%23000000&foregroundOpacity=1&imageFormat=png&rotation=0"
+        }
+        alt="Pokemon"
+        style={{ width: 50, height: 50 }}
+      />
     ),
   },
 ];
